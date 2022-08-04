8482d76f7af4fa858c91254d0991e551a40212be
ClearlyJustSomeGuy <99519986+ClearlyJustSomeGuy@users.noreply.github.com>
2022-08-04 14:31:06 -0600

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    if (typeof numOne != 'number' || typeof numTwo != 'number') {
+        return 'ERROR';
+    }
+    if (numOne < 0 || numTwo < 0) {
+        return 'ERROR';
+    }
+    if (numOne > numTwo) {
+        let tmp = numOne;
+        numOne = numTwo;
+        numTwo = tmp;
+    }
+    for (i = numOne; i <= numTwo; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
