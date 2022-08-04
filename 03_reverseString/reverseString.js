51da5505374ddc7c4b7737d839fed5fee8ce3352
ClearlyJustSomeGuy <99519986+ClearlyJustSomeGuy@users.noreply.github.com>
2022-08-04 11:44:27 -0600

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = [];
+    for (i = 0; i < string.length; i++) {
+        newString.unshift(string[i]);
+        // console.log(newString);
+    }
+    return newString.join('');
 
 };
 
