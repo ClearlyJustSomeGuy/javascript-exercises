00e6de80da5c1a93f33965717862cda422352620
ClearlyJustSomeGuy <99519986+ClearlyJustSomeGuy@users.noreply.github.com>
2022-08-05 08:54:48 -0600

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let array = [];
+    let lowerString = string.toLowerCase();
 
+    //Strip out non-lowerAlphas
+    for (let i = 0; i < lowerString.length; i++) {
+        if (lowerString.charCodeAt(i) < 123 && 
+            lowerString.charCodeAt(i) > 96) {
+                array.push(lowerString[i]);
+        }
+    }
+    // console.log(array);
+
+    let len = Math.floor(array.length / 2);
+    for (let i = 0; i < len; i++) {
+        if (array[i] !== array[array.length - i - 1]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
