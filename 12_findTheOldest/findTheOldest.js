2d77c00c781daabd7f733122f82696143d1c5730
ClearlyJustSomeGuy <99519986+ClearlyJustSomeGuy@users.noreply.github.com>
2022-08-08 15:18:20 -0600

    Pass test 1 of exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let ages = [];
+    for (const person of people) {
+        ages.push(getAge(person.yearOfBirth, person.yearOfDeath));
+    }
+    let max = Math.max(...ages);
+    
+
+    for (const person of people) {
+        if (max === getAge(person.yearOfBirth, person.yearOfDeath)) {
+            console.log(person);
+            return person;
+        }
+    }
+
 
 };
+
+function getAge(birthYear, deathYear) {
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
